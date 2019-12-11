9fd8daffc50ff2b8323975470089f44e008abd74
takuma hidaka <hidakatakuma.tukuba@gmail.com>
2019-12-11 15:55:38 +0900

    新しいroutsを作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -38,6 +38,10 @@ class CommentsController < ApplicationController
     @comment.destroy
   end
 
+  def get_from_url
+    render json: @comment.where(url: param[:url])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "comments/get_from_url" => "comments#get_from_url"
   resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
